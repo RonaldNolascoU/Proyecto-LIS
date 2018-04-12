c00bb69a1c28a12cf4128f50a3bc960d23b89fa8
Kevin Romero <a1299r5@gmail.com>
2018-04-12 09:06:36 -0600

    migracion

---
+++ database/migrations/2018_04_12_150528_create_clientes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('PrimerNombre', 15);
+            $table->string('SegundoNombre', 15)->nullable();
+            $table->string('PrimerApellido', 15);
+            $table->string('SegundoApellido', 15)->nullable();
+            $table->string('DUI',10);
+            $table->string('NIT',15);
+            $table->string('telefono',9);
+            $table->string('correo',50)->unique();
+            $table->text('clave');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
